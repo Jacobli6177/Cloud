8d9a30198f67f6dbda008a39d7a5f7879ef39db9
Jacobli6177 <101788026+Jacobli6177@users.noreply.github.com>
2025-07-14 01:33:27 -0400

    check error for accout creations

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,5 +1,37 @@
 "use server";
 
+import { createAdminClient } from "@/appwrite";
+import { appwriteConfig } from "@/appwrite/config";
+import { Query, ID } from "node-appwrite";
+
+
+const getUserbyEmail = async (email: string) => {
+    const { databases } = await createAdminClient();
+
+    const result = await databases.listDocuments(
+        appwriteConfig.databaseId,
+        appwriteConfig.usersCollectionId,
+        [Query.equal("email", [email])]
+    );
+    return result.total > 0 ? result.documents[0]: null
+} 
+const handleError = (error: unknown, message:string) => {
+    console.log(error, message);
+    throw error
+}
+
+const sendEmailOTP = async ({email}: {email: string}) => {
+    const { account } = await createAdminClient();
+
+    try {
+        const session = await account.createEmailToken(ID.unique(), email);
+
+        return session.userId;
+    } catch(error) {
+        handleError(error, "Failed to send email OTP")
+    };
+}
+
 const createAccount = async ({
     fullName,
     email,
@@ -8,4 +40,8 @@ const createAccount = async ({
     email: string;
 }) => {
     const existingUser = await getUserbyEmail(email);
+
+    const accountID = await sendEmailOTP({ email })
+
+    
 };
