ab6fff1aae5f53573109a83357a261a30f96023a
Jacobli6177 <101788026+Jacobli6177@users.noreply.github.com>
2025-07-13 23:52:15 -0400

    user action like email

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,11 @@
+"use server";
+
+const createAccount = async ({
+    fullName,
+    email,
+}: {
+    fullName: string;
+    email: string;
+}) => {
+    const existingUser = await getUserbyEmail(email);
+};
