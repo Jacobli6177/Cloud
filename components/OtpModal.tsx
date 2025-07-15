ab148949a9c8d361ef799295797190a01f614459
Jacobli6177 <101788026+Jacobli6177@users.noreply.github.com>
2025-07-15 01:45:37 -0400

    adding tailwind to OTP

--- components/OtpModal.tsx
+++ components/OtpModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   AlertDialog,
   AlertDialogAction,
@@ -11,18 +11,74 @@ import {
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
 
-const OtpModal = () => {
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp"
+import Image from 'next/image';
+
+const OtpModal = ({
+  accountId,
+  email,
+}: {
+  accountId: string;
+  email: string;
+}) => {
+  const router = useRouter();
+  const [isOpen, setIsOpen] = useState(true);
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    console.log({ accountId, password });
+
+    try {
+      const sessionId = await verifySecret({ accountId, password });
+
+      console.log({ sessionId });
+
+      if (sessionId) router.push("/");
+    } catch (error) {
+      console.log("Failed to verify OTP", error);
+    }
+
+    setIsLoading(false);
+  };
+
+  const handleResendOtp = async () => {
+    await sendEmailOTP({ email });
+  };
+
   return (
-   <AlertDialog>
-  <AlertDialogTrigger>Open</AlertDialogTrigger>
-  <AlertDialogContent>
-    <AlertDialogHeader>
-      <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-      <AlertDialogDescription>
-        This action cannot be undone. This will permanently delete your account
-        and remove your data from our servers.
+   <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
+  <AlertDialogContent className='shad-alert-dialog'>
+    <AlertDialogHeader className='relative flex justify-center'>
+      <AlertDialogTitle className='text-center'>
+        Enter Your OTP
+        <Image src="/assets/icons/close-dark.svg" alt='close' width={20} height={20} onClick={() => setIsOpen(false)} className='otp-close-button'/>
+      </AlertDialogTitle>
+      <AlertDialogDescription className='subtitle-2 text-center text-light-100'>
+        We've sent a code to <span>{email}</span>
       </AlertDialogDescription>
     </AlertDialogHeader>
+    <InputOTP maxLength={6}>
+      <InputOTPGroup>
+        <InputOTPSlot index={0} />
+        <InputOTPSlot index={1} />
+        <InputOTPSlot index={2} />
+      </InputOTPGroup>
+      <InputOTPSeparator />
+      <InputOTPGroup>
+        <InputOTPSlot index={3} />
+        <InputOTPSlot index={4} />
+        <InputOTPSlot index={5} />
+      </InputOTPGroup>
+    </InputOTP>
     <AlertDialogFooter>
       <AlertDialogCancel>Cancel</AlertDialogCancel>
       <AlertDialogAction>Continue</AlertDialogAction>
