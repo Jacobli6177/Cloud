974a16e43c54c0722de34b050441fccd1fabdc59
Jacobli6177 <101788026+Jacobli6177@users.noreply.github.com>
2025-07-15 23:31:57 -0400

    importing more feature in the otp email and page

--- components/OtpModal.tsx
+++ components/OtpModal.tsx
@@ -18,6 +18,7 @@ import {
   InputOTPSlot,
 } from "@/components/ui/input-otp"
 import Image from 'next/image';
+import { Button } from './ui/button';
 
 const OtpModal = ({
   accountId,
@@ -63,25 +64,34 @@ const OtpModal = ({
         <Image src="/assets/icons/close-dark.svg" alt='close' width={20} height={20} onClick={() => setIsOpen(false)} className='otp-close-button'/>
       </AlertDialogTitle>
       <AlertDialogDescription className='subtitle-2 text-center text-light-100'>
-        We've sent a code to <span>{email}</span>
+        We&apos;ve sent a code to <span className='pl-1 text-brand'>{email}</span>
       </AlertDialogDescription>
     </AlertDialogHeader>
-    <InputOTP maxLength={6}>
-      <InputOTPGroup>
-        <InputOTPSlot index={0} />
-        <InputOTPSlot index={1} />
-        <InputOTPSlot index={2} />
-      </InputOTPGroup>
-      <InputOTPSeparator />
-      <InputOTPGroup>
-        <InputOTPSlot index={3} />
-        <InputOTPSlot index={4} />
-        <InputOTPSlot index={5} />
+    <InputOTP maxLength={6} value={password} onChange={setPassword}>
+      <InputOTPGroup className='shad-otp'>
+        <InputOTPSlot index={0} className='shad-otp-slot'/>
+        <InputOTPSlot index={1} className='shad-otp-slot'/>
+        <InputOTPSlot index={2} className='shad-otp-slot'/>
+        <InputOTPSlot index={3} className='shad-otp-slot'/>
+        <InputOTPSlot index={4} className='shad-otp-slot'/>
+        <InputOTPSlot index={5} className='shad-otp-slot'/>
       </InputOTPGroup>
     </InputOTP>
     <AlertDialogFooter>
-      <AlertDialogCancel>Cancel</AlertDialogCancel>
-      <AlertDialogAction>Continue</AlertDialogAction>
+      <div className='flex w-full flex-col gap-4'>
+          <AlertDialogAction onClick={handleSubmit} className='shad-submit-btn h-12' type="button">
+            Submit
+            {isLoading && <Image src="/assets/icons/loader.svg" alt="loader" width={24} height={24} className='ml-2 animate-spin'/>}
+          </AlertDialogAction>
+
+          <div>
+            Didn't get a code ?
+            <Button type='button' variant="link" className='pl-1 text-brand' onClick={handleResendOtp()}>
+              Click to resend
+            </Button>
+          </div>
+
+      </div>
     </AlertDialogFooter>
   </AlertDialogContent>
 </AlertDialog>
