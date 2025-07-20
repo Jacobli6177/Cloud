30c19b8e1e939ae17f286c0cfceb51f3a5968e6d
Jacobli6177 <101788026+Jacobli6177@users.noreply.github.com>
2025-07-19 23:39:28 -0400

    created the uploader for files

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+'use client';
 
-const FileUploader = () => {
+import React, {useCallback} from 'react'
+import {useDropzone} from 'react-dropzone'
+import { Button } from './ui/button';
+import { cn } from '@/lib/utils';
+import Image from 'next/image';
+
+interface Props {
+  ownerId: string,
+  accoundId: string,
+  className?: string,
+}
+
+const FileUploader = ({ownerId, accountId, className}: Props) => {
+  const onDrop = useCallback(acceptedFiles => {
+    // Do something with the files
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
   return (
-    <div>
-      
+    <div {...getRootProps()} className='cursor-pointer'>
+      <input {...getInputProps()} />
+      <Button type='button' className={cn("uploader-button", className)}>
+        <Image src="/assets/icons/upload.svg" alt='upload' width={24} height={24}/>
+      </Button>
+      {
+        isDragActive ?
+          <p>Drop the files here ...</p> :
+          <p>Drag 'n' drop some files here, or click to select files</p>
+      }
     </div>
   )
 }
