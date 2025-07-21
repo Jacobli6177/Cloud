bed35e3f59c96957e0bbfb02c864eefa5e9488f9
Jacobli6177 <101788026+Jacobli6177@users.noreply.github.com>
2025-07-20 22:46:45 -0400

    implemnted a cancel on the uploader

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -20,6 +20,16 @@ const FileUploader = ({ownerId, accountId, className}: Props) => {
         setFiles(acceptedFiles);
       }, [])
 
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({onDrop});
+
+  const handleRemoveFile = (
+    e: React.MouseEvent<HTMLImageElement, MouseEvent>,
+    fileName: string,
+  ) => {
+    e.stopPropagation();
+    setFiles((prevFiles) => prevFiles.filter((file) => file.name !== fileName));
+  };
+
   return (
     <div {...getRootProps()} className='cursor-pointer'>
       <input {...getInputProps()} />
@@ -37,13 +47,19 @@ const FileUploader = ({ownerId, accountId, className}: Props) => {
             <li
             key={`${file.name}-${index}`}
             className='uploader-preview-item'>
-              <div> 
+              <div className='flex items-center gap-3'> 
                 <Thumbnail 
                   type={type}
                   extension={extension}
                   url={convertFileToUrl(file)}
                 />
+                <div className='preview-item-name'>
+                  {file.name}
+                  <Image src="/assets/icons/file-loader.gif" width={80} height={26} alt='loader' />
+                </div>
               </div>
+
+              <Image src="/assets/icons/remove.svg" width={24} height={24} alt='Remove' onClick={(e) => handleRemoveFile(e, file.name)} />
             </li>
           )
         })}
@@ -52,7 +68,7 @@ const FileUploader = ({ownerId, accountId, className}: Props) => {
       {
         isDragActive ?
           <p>Drop the files here ...</p> :
-          <p>Drag 'n' drop some files here, or click to select files</p>
+          <p>Drag drop some files here, or click to select files</p>
       }
     </div>
   )
