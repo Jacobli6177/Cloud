6e4b999dbfdfe649351036813f629219beb1306e
Jacobli6177 <101788026+Jacobli6177@users.noreply.github.com>
2025-07-23 15:29:06 -0400

    uploaded appwrite to correct location

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,7 +1,7 @@
 "use server";
 
-import { createAdminClient, createSessionClient } from "@/appwrite";
-import { appwriteConfig } from "@/appwrite/config";
+import { createAdminClient, createSessionClient } from "@/lib/appwrite";
+import { appwriteConfig } from "@/lib/appwrite/config";
 import { Query, ID } from "node-appwrite";
 import { parseStringify } from "@/lib/utils";
 import { cookies } from "next/headers";
