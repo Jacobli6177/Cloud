45fd0f4eff80030981ec63085d9ca9fca9bf90f4
Jacobli6177 <101788026+Jacobli6177@users.noreply.github.com>
2025-08-03 23:50:32 -0400

    fixed and implemented filedetails

--- components/actionsModalContent.tsx
+++ components/actionsModalContent.tsx
@@ -3,17 +3,15 @@ import React from 'react'
 import Thumbnail from './Thumbnail'
 import FormattedDateTime from './FormattedDateTime'
 
-const ImageThumbnail = ({ file }: {file: Models.Document}) => {
-  <div className='file-details-thumbnail'>
+const ImageThumbnail = ({ file }: { file: Models.Document }) => (
+  <div className="file-details-thumbnail">
     <Thumbnail type={file.type} extension={file.extension} url={file.url} />
-    <div className='flex flex-col'>
-      <p className='subtitle-2 mb-1'>
-        {file.name}
-        <FormattedDateTime date={file.$createdAt} className='caption'/>
-      </p>
+    <div className="flex flex-col">
+      <p className="subtitle-2 mb-1">{file.name}</p>
+      <FormattedDateTime date={file.$createdAt} className="caption" />
     </div>
   </div>
-}
+);
 
 const DetailRow = ({ label, value }: { label: string; value: string }) => (
   <div className="flex">
@@ -22,10 +20,16 @@ const DetailRow = ({ label, value }: { label: string; value: string }) => (
   </div>
 );
 
-export const FileDetails = ({ file }: {file: Models.Document}) => {
-  return 
+export const FileDetails = ({ file }: { file: Models.Document }) => {
+  return (
     <>
       <ImageThumbnail file={file} />
+      <div className="space-y-4 px-2 pt-2">
+        <DetailRow label="Format:" value={file.extension} />
+        <DetailRow label="Size:" value={convertFileSize(file.size)} />
+        <DetailRow label="Owner:" value={file.owner.fullName} />
+        <DetailRow label="Last edit:" value={formatDateTime(file.$updatedAt)} />
+      </div>
     </>
+  );
 };
-
