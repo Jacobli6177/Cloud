03486bb612e463ec21f61eb66f346d6b3215dc9f
Jacobli6177 <101788026+Jacobli6177@users.noreply.github.com>
2025-08-03 23:48:57 -0400

    implemented detail rows

--- components/actionsModalContent.tsx
+++ components/actionsModalContent.tsx
@@ -1,6 +1,7 @@
 import { Models } from 'node-appwrite'
 import React from 'react'
 import Thumbnail from './Thumbnail'
+import FormattedDateTime from './FormattedDateTime'
 
 const ImageThumbnail = ({ file }: {file: Models.Document}) => {
   <div className='file-details-thumbnail'>
@@ -8,10 +9,18 @@ const ImageThumbnail = ({ file }: {file: Models.Document}) => {
     <div className='flex flex-col'>
       <p className='subtitle-2 mb-1'>
         {file.name}
+        <FormattedDateTime date={file.$createdAt} className='caption'/>
       </p>
     </div>
   </div>
 }
+
+const DetailRow = ({ label, value }: { label: string; value: string }) => (
+  <div className="flex">
+    <p className="file-details-label text-left">{label}</p>
+    <p className="file-details-value text-left">{value}</p>
+  </div>
+);
 
 export const FileDetails = ({ file }: {file: Models.Document}) => {
   return 
