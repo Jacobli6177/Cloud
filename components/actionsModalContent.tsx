2e3e9f9dfb0d78f98477a112522e9e9caea67f59
Jacobli6177 <101788026+Jacobli6177@users.noreply.github.com>
2025-08-03 23:46:42 -0400

    finihsed modifying image thumbnail

--- components/actionsModalContent.tsx
+++ components/actionsModalContent.tsx
@@ -1,13 +1,22 @@
 import { Models } from 'node-appwrite'
 import React from 'react'
+import Thumbnail from './Thumbnail'
 
 const ImageThumbnail = ({ file }: {file: Models.Document}) => {
-  <div>
-    
+  <div className='file-details-thumbnail'>
+    <Thumbnail type={file.type} extension={file.extension} url={file.url} />
+    <div className='flex flex-col'>
+      <p className='subtitle-2 mb-1'>
+        {file.name}
+      </p>
+    </div>
   </div>
 }
 
 export const FileDetails = ({ file }: {file: Models.Document}) => {
-  return <div>FileDetails</div>
-}
+  return 
+    <>
+      <ImageThumbnail file={file} />
+    </>
+};
 
