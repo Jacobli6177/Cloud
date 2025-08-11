b3261a03270bb4c95ce18caf7ecd66ee63f37e88
Jacobli6177 <101788026+Jacobli6177@users.noreply.github.com>
2025-08-10 20:11:23 -0400

    implementing srot element

--- lib/actions/file.action.ts
+++ lib/actions/file.action.ts
@@ -24,6 +24,14 @@ const createQueries = (currentUser: Models.Document, types:string[], searchText:
   if (types.length > 0) queries.push(Query.equal('type', types));
   if (searchText) queries.push(Query.contains("name", searchText));
   if (limit) queries.push(Query.limit(limit));
+
+  if (sort) {
+    const [sortBy, orderBy] sort.split("-");
+
+    queries.push(
+      orderBy === "asc" ? Query.orderAsc(sortBy) : Query.orderDesc(sortBy),
+    );
+  }
     return queries
 }
 
