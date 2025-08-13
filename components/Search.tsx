ff6283df6b3359594bae5d8d226985825ff15f44
Jacobli6177 <101788026+Jacobli6177@users.noreply.github.com>
2025-08-13 00:29:12 -0400

    implemnt query and fetch files

--- components/Search.tsx
+++ components/Search.tsx
@@ -9,12 +9,29 @@ const Search = () => {
   const [query, setQuery] = useState('')
   const searchParams = useSearchParams();
   const searchQuery = searchParams.get("query") || "";
+  const [debouncedQuery] = useDebounce(query, 300);
+
+  useEffect(() => {
+    const fetchFiles = async () => {
+      if (debouncedQuery.length === 0) {
+        setResults([]);
+        setOpen(false);
+        return router.push(path.replace(searchParams.toString(), ""));
+      }
+
+      const files = await getFiles({ types: [], searchText: debouncedQuery });
+      setResults(files.documents);
+      setOpen(true);
+    };
+
+    fetchFiles();
+  }, [debouncedQuery]);
   useEffect(() => {
     if (!searchQuery) {
       setQuery("")
     }
   }, [searchQuery])
-
+ 
   return (
     <div className='search'>
       <div className='search-input-wrapper'>
