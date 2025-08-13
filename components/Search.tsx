3bc893acf92e31e7f43979b8c0029e0cc7f681ab
Jacobli6177 <101788026+Jacobli6177@users.noreply.github.com>
2025-08-12 21:14:37 -0400

    creating the search page

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import Image from 'next/image'
+import React, { useState } from 'react'
+import { Input } from './ui/input'
 
 const Search = () => {
+  const [query, setQuery] = useState('')
+
   return (
-    <div>
-      
+    <div className='search'>
+      <div className='search-input-wrapper'>
+        <Image 
+          src="/assets/icons/search.svg"
+          alt='search'
+          width={24}
+          height={24}
+        /> 
+        <Input />
+      </div>
     </div>
   )
 }
