63b808f04c5bab00adb2bb212b053ff2b75d1a85
Jacobli6177 <101788026+Jacobli6177@users.noreply.github.com>
2025-08-12 21:31:38 -0400

    implemnted search features

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,9 +1,19 @@
+"use client";
+
 import Image from 'next/image'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Input } from './ui/input'
+import { useSearchParams } from 'next/navigation';
 
 const Search = () => {
   const [query, setQuery] = useState('')
+  const searchParams = useSearchParams();
+  const searchQuery = searchParams.get("query") || "";
+  useEffect(() => {
+    if (!searchQuery) {
+      setQuery("")
+    }
+  }, [searchQuery])
 
   return (
     <div className='search'>
@@ -14,7 +24,9 @@ const Search = () => {
           width={24}
           height={24}
         /> 
-        <Input />
+        <Input value={query} placeholder='Search...' 
+        className='search-input' 
+        onChange={(e) => setQuery(e.target.value)}/>
       </div>
     </div>
   )
