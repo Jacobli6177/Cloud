7d1fa69ea7dcca148c82e77b05de653ab614d6ee
Jacobli6177 <101788026+Jacobli6177@users.noreply.github.com>
2025-08-13 23:36:24 -0400

    updates files for search components

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,14 +1,23 @@
 "use client";
 
-import Image from 'next/image'
-import React, { useEffect, useState } from 'react'
-import { Input } from './ui/input'
-import { useSearchParams } from 'next/navigation';
+import React, { useEffect, useState } from "react";
 
+import Image from "next/image";
+import { Input } from "@/components/ui/input";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { getFiles } from "@/lib/actions/file.actions";
+import { Models } from "node-appwrite";
+import Thumbnail from "@/components/Thumbnail";
+import FormattedDateTime from "@/components/FormattedDateTime";
+import { useDebounce } from "use-debounce";
 const Search = () => {
-  const [query, setQuery] = useState('')
+  const [query, setQuery] = useState("");
   const searchParams = useSearchParams();
   const searchQuery = searchParams.get("query") || "";
+  const [results, setResults] = useState<Models.Document[]>([]);
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
+  const path = usePathname();
   const [debouncedQuery] = useDebounce(query, 300);
 
   useEffect(() => {
